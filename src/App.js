0343fd2bffbda7144f42bca2ccea4ce34ac25162
roum02 <roum02@ewhain.net>
2021-08-10 23:39:25 +0900

    5장 끝

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
-import Vaildation from "./Vaildation";
+import React, { Component } from "react";
+import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return <Vaildation />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
 
 export default App;
---
+++ src/ScrollBox.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+class ScrollBox extends Component {
+  // scrollTop: 세로 스크롤바 위치
+  // scrollHeight: 스크롤이 있는 박스 안의 div 높이
+  // clientHeight: 스크롤이 있는 박스의 높이
+
+  scrollToBottom = () => {
+    // 비구조화 할당 문법 사용
+    const { scrollHeight, clientHeight } = this.box;
+    this.box.scrollTop = scrollHeight - clientHeight;
+  };
+
+  render() {
+    const style = {
+      border: "1px solid black",
+      height: "300px",
+      width: "300px",
+      overflow: "auto",
+      position: "relative",
+    };
+
+    const innerStyle = {
+      width: "100%",
+      height: "650px",
+      background: "linear-gradient(white, black)",
+    };
+
+    return (
+      <div
+        style={style}
+        ref={(ref) => {
+          this.box = ref;
+        }}
+      >
+        <div style={innerStyle} />
+      </div>
+    );
+  }
+}
+
+export default ScrollBox;
