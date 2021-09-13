d6f7784e97f8d16339247adee6095a3a73b9342f
roum02 <roum02@ewhain.net>
2021-09-13 23:01:30 +0900

    10장 완료

--- src/App.js
+++ src/App.js
@@ -25,7 +25,10 @@ const App = () => {
     },
   ]);
 
+  // ID값은 렌더링되는 정보가 아님, 값이 바뀐다고 해서 컴포넌트가 리렌더링 될 필요 없음
   const nextID = useRef(4);
+  // useCallback으로 감싸는 이유: 컴포넌트의 성능을 아끼기 위함.
+  // props로 전달해야 할 함수를 만들 때는 useCallback을 사용해 감싸도록 하자.
   const onInsert = useCallback(
     (text) => {
       const todo = {
@@ -38,10 +41,29 @@ const App = () => {
     },
     [todos]
   );
+
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos]
+  );
+
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo
+        )
+      );
+    },
+    [todos]
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
