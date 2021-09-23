7cf0e1db740f9415b1952351e555bc436e460143
roum02 <roum02@ewhain.net>
2021-09-23 22:09:32 +0900

    컴포넌트 최적화, 11장 마무리

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useCallback } from "react";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.scss";
+import { List } from "react-virtualized";
 
 const TodoList = ({ todos, onRemove, onToggle }) => {
-  return (
-    <div className="TodoList">
-      {todos.map((todo) => (
-        // 여러 종류의 값을 전달해야 하는 경우
-        // 객체로 통째로 전달하는 편이 성능 최적화에 편리
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </div>
+      );
+    },
+    [onRemove, onToggle, todos]
+  );
+
+  return (
+    <List
+      className="TodoList"
+      width={495}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{ outline: "none" }}
+    />
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
